a38bc7e09504dbb885b3f14ac9ae483c92453a48
User <user@UserdeMacBook-Pro.local>
2020-03-26 21:15:41 +0800

    [FEATURE] use Elastic UI

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { Input } from 'semantic-ui-react'
-import { Slider } from '@material-ui/core';
+import {
+  EuiRange,
+  EuiForm,
+  EuiPopover,
+  EuiFlexGroup,
+  EuiButton,
+  EuiFlexItem,
+  EuiFormRow,
+  EuiFieldText,
+} from '@elastic/eui';
 import styles from './Home.css';
-import { handleStringChange } from "@blueprintjs/docs-theme";
-// import { Link } from 'react-router-dom';
-// import routes from '../constants/routes.json';
 
 const Container = styled.div`
   position: relative;
@@ -34,28 +39,28 @@ const Overlay = styled.div`
   bottom: 0;
   right: 0;
   position: absolute;
-  z-index: -1;  
+  z-index: -1;
 `;
 const ControlPanel = styled.div`
-  bottom: 10px;
-  position: absolute;
-  width: 100%;
-  height: 48px;
-  padding: 8px 48px;
-
-  /* display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 24px; */
   display: flex;
   justify-content: space-around;
   align-items: center;
 `;
+
+const StyledEuiPopover = styled(EuiPopover)`
+  bottom: 10px;
+  left: 10px;
+  position: absolute;
+  width: 360px;
+  height: auto;
+`;
+
 export default function Home() {
-  // token = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoicGVyc29uYWxfYWNjZXNzX3Rva2VuIiwiY2xpZW50X2lkIjoiNWU3YTAyYjA3NmNjMjA4YmU1NDljOWE1Iiwic2NvcGUiOiIiLCJpYXQiOjE1ODUwNTQzODQsImV4cCI6MTkwMDYyMzY0NCwiaXNzIjoiemVwbGluOmFwaS56ZXBsaW4uaW8iLCJzdWIiOiI1YzljMTg3NDE2ZDRlZDc3NmJiNTAzNTciLCJqdGkiOiJjZDQwZjFhNC05NzE0LTQzZTItYmNkMS02OTEzYTVjMTRjOWEifQ.3xLhOpEubiXO0GDM2Z6KJ2E9UxRpfjVlqa5Xerr9k5M
   const projectId = '5d3961af5fd2632e5d62fc34';
   const [opacity, SetOpacity] = useState(50);
   const [token, SetToken] = useState('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoicGVyc29uYWxfYWNjZXNzX3Rva2VuIiwiY2xpZW50X2lkIjoiNWU3YTAyYjA3NmNjMjA4YmU1NDljOWE1Iiwic2NvcGUiOiIiLCJpYXQiOjE1ODUwNTQzODQsImV4cCI6MTkwMDYyMzY0NCwiaXNzIjoiemVwbGluOmFwaS56ZXBsaW4uaW8iLCJzdWIiOiI1YzljMTg3NDE2ZDRlZDc3NmJiNTAzNTciLCJqdGkiOiJjZDQwZjFhNC05NzE0LTQzZTItYmNkMS02OTEzYTVjMTRjOWEifQ.3xLhOpEubiXO0GDM2Z6KJ2E9UxRpfjVlqa5Xerr9k5M');
   const [screenID, SetScreenID] = useState('5e55658227cd32677b1e6b77');
+  const [showControlPanel, setShowControlPanel] = useState(false);
   useEffect(() => {
     fetch(`https://api.zeplin.dev/v1/projects/${projectId}/screens/${screenID}`, { 
       method: 'GET', 
@@ -77,49 +82,96 @@ export default function Home() {
       console.error(error);
     });
   }, [screenID])
+  const ControlPanelButton = (
+    <EuiButton
+      iconSide="right"
+      fill
+      size="s"
+      iconType="arrowDown"
+      onClick={() => setShowControlPanel(!showControlPanel)}>
+      Toggle ControlPanel
+    </EuiButton>
+  );
   return (
     <Container className={styles.container} data-tid="container">
-      <Overlay>
+      <Overlay />
+      <StyledEuiPopover
+        id="control-Popover"
+        ownFocus
+        button={ControlPanelButton}
+        isOpen={showControlPanel}
+        closePopover={() => setShowControlPanel(false)}
+      >
+        <ControlPanel>
+          <EuiForm component="form">
+            <EuiFlexGroup style={{ maxWidth: 600 }}>
+              <EuiFlexItem id="overlay-opacity-control">
+                {/* <EuiFormRow label="Scale" helpText="">
+                    <EuiRange
+                      id={'123'}
+                      min={0}
+                      max={100}
+                      step={1}
+                      value={opacity}
+                      onChange={e => {
+                        const newV = parseInt(e.target.value, 10)
+                        SetOpacity(newV);
+                        document.documentElement.style.setProperty('--overlay-opacity', `${newV / 100}`);
+                      }}
+                      showLabels
+                  />
+                </EuiFormRow> */}
+                <EuiFormRow id="overlay-opacity-control" label="Opacity" helpText="">
+                  <EuiRange
+                    id={'123'}
+                    min={0}
+                    max={100}
+                    step={1}
+                    showInput
+                    value={opacity}
+                    onChange={e => {
+                      const newV = parseInt(e.target.value, 10)
+                      SetOpacity(newV);
+                      document.documentElement.style.setProperty('--overlay-opacity', `${newV / 100}`);
+                    }}
+                    showLabels
+                  />
+                  </EuiFormRow>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+            <EuiFlexGroup style={{ maxWidth: 600 }}>
+              <EuiFlexItem>
+                <EuiFormRow id="section-zeplin-projectID" label="Project ID" helpText="">
+                  <EuiFieldText
+                    compressed
+                    placeholder="get it from the web"
+                    value={projectId}
+                    readOnly
+                  />
+                </EuiFormRow>
 
-      </Overlay>
-      <ControlPanel>
-          {/* <Slider
-            defaultValue={50}
-            aria-labelledby="discrete-slider"
-            valueLabelDisplay="auto"
-            step={1}
-            min={10}
-            max={100}
-            onChange={(event: any, newValue: any) => {
-              SetOpacity(newValue);
-              document.documentElement.style.setProperty('--overlay-opacity', `${newValue / 100}`);
-            }}
-          /> */}
-          <input
-            type='range'
-            step={1}
-            min={10}
-            max={100}
-            value={opacity}
-            onChange={(event) => {
-              const newV = parseInt(event.target.value, 10)
-              SetOpacity(newV);
-              document.documentElement.style.setProperty('--overlay-opacity', `${newV / 100}`);
-            }}
-          />
-          <Input
-            size='small'
-            value={screenID}
-            onChange={handleStringChange((tagValue) => SetScreenID(tagValue))}
-            placeholder='Enter screenID...'
-          />
-          <Input
-            size='small'
-            value={token}
-            onChange={handleStringChange((tagValue) => SetToken(tagValue))}
-            placeholder='Enter token...'
-          />
-      </ControlPanel>
+                <EuiFormRow id="section-zeplin-screenID" label="Screen ID">
+                  <EuiFieldText
+                    compressed
+                    placeholder="get it from the web"
+                    value={screenID}
+                    onChange={(e) => SetScreenID(e.target.value)}
+                  />
+                </EuiFormRow>
+
+                <EuiFormRow id="section-zeplin-token" label="Token" helpText="">
+                  <EuiFieldText
+                    compressed
+                    placeholder="get it from the develop panel on zeplin"
+                    value={token}
+                    onChange={(e) => SetToken(e.target.value)}
+                  />
+                </EuiFormRow>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiForm>
+        </ControlPanel>  
+      </StyledEuiPopover>
     </Container>
   );
 }
