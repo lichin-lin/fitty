d7f0338c8285394c9af929e97de443ff3bbb3619
User <user@UserdeMacBook-Pro.local>
2020-03-27 22:00:57 +0800

    [FEATURE] use figma api

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -57,32 +57,52 @@ const StyledEuiPopover = styled(EuiPopover)`
 `;
 
 export default function Home() {
-  const projectId = '5d3961af5fd2632e5d62fc34';
   const [opacity, SetOpacity] = useState(50);
+  // Zeplin
+  const projectId = '5d3961af5fd2632e5d62fc34';
   const [token, SetToken] = useState('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoicGVyc29uYWxfYWNjZXNzX3Rva2VuIiwiY2xpZW50X2lkIjoiNWU3YTAyYjA3NmNjMjA4YmU1NDljOWE1Iiwic2NvcGUiOiIiLCJpYXQiOjE1ODUwNTQzODQsImV4cCI6MTkwMDYyMzY0NCwiaXNzIjoiemVwbGluOmFwaS56ZXBsaW4uaW8iLCJzdWIiOiI1YzljMTg3NDE2ZDRlZDc3NmJiNTAzNTciLCJqdGkiOiJjZDQwZjFhNC05NzE0LTQzZTItYmNkMS02OTEzYTVjMTRjOWEifQ.3xLhOpEubiXO0GDM2Z6KJ2E9UxRpfjVlqa5Xerr9k5M');
   const [screenID, SetScreenID] = useState('5e55658227cd32677b1e6b77');
+  // Figma
+  const [figmaFileID, SetFigmaFileID] = useState('MtR6sYoPw9qnT9VHT6Pwlv');
+  const [figmaToken, SetFigmaToken] = useState('39373-5d3aee3e-c57c-4f62-a29c-92af3084aa7f');
+  const [figmaFrameID, SetFigmaFrameID] = useState('5e55658227cd32677b1e6b77');
   const [showControlPanel, setShowControlPanel] = useState(false);
+
+  // useEffect(() => {
+  //   fetch(`https://api.zeplin.dev/v1/projects/${projectId}/screens/${screenID}`, { 
+  //     method: 'GET', 
+  //     headers: {
+  //       'Authorization': `Bearer ${token}`, 
+  //       'Accept': 'application/json',
+  //     }
+  //   }).then((res) => {
+  //     return res.json()
+  //   }).then((resJson) => {
+  //     if(resJson.image && resJson.image.original_url) {
+  //       console.log('set to background!', resJson.image);
+  //       const h = 100 * resJson.image.height / resJson.image.width
+  //       document.documentElement.style.setProperty('--overlay-image', `url('${resJson.image.original_url}')`);
+  //       document.documentElement.style.setProperty('--overlay-width', `100%`);
+  //       document.documentElement.style.setProperty('--overlay-height', `${h}vw`);
+  //     }
+  //   }).catch((error) => {
+  //     console.error(error);
+  //   });
+  // }, [screenID])
+
   useEffect(() => {
-    fetch(`https://api.zeplin.dev/v1/projects/${projectId}/screens/${screenID}`, { 
-      method: 'GET', 
+    fetch(`https://api.figma.com/v1/files/${figmaFileID}` , {
+      method: 'GET',
       headers: {
-        'Authorization': `Bearer ${token}`, 
-        'Accept': 'application/json',
+          'X-Figma-Token': figmaToken
       }
     }).then((res) => {
       return res.json()
     }).then((resJson) => {
-      if(resJson.image && resJson.image.original_url) {
-        console.log('set to background!', resJson.image);
-        const h = 100 * resJson.image.height / resJson.image.width
-        document.documentElement.style.setProperty('--overlay-image', `url('${resJson.image.original_url}')`);
-        document.documentElement.style.setProperty('--overlay-width', `100%`);
-        document.documentElement.style.setProperty('--overlay-height', `${h}vw`);
-      }
-    }).catch((error) => {
-      console.error(error);
+      console.log(resJson);
     });
-  }, [screenID])
+
+  }, [figmaFrameID])
   const ControlPanelButton = (
     <EuiButton
       iconSide="right"
