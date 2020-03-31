d2b3123ebf31cb696af14775300aeb3ac52fdeea
lichin-lin <vic20087cjimlin@gmail.com>
2020-04-01 00:50:17 +0800

    [FEATURE] support figma and scaling

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -17,6 +17,8 @@ const Container = styled.div`
   position: relative;
   width: 100%;
   height:100%;
+  display: flex;
+  justify-content: flex-start;
 
   h2 {
     font-size: 5rem;
@@ -28,7 +30,8 @@ const Container = styled.div`
 const Overlay = styled.div`
   width: var(--overlay-width);
   height: var(--overlay-height);
-
+  min-width: var(--overlay-width);
+  min-height: var(--overlay-height);
   content: "";
   background-image: var(--overlay-image);
   background-position: center;
@@ -39,7 +42,7 @@ const Overlay = styled.div`
   left: 0;
   bottom: 0;
   right: 0;
-  position: absolute;
+  /* position: absolute; */
   z-index: -1;
 `;
 const ControlPanel = styled.div`
@@ -51,13 +54,15 @@ const ControlPanel = styled.div`
 const StyledEuiPopover = styled(EuiPopover)`
   bottom: 16px;
   left: 16px;
-  position: absolute;
+  position: fixed;
   width: 480px;
   height: auto;
 `;
 
 export default function Home() {
   const [opacity, SetOpacity] = useState(50);
+  const [scale, SetScale] = useState(100);
+  const [currentTab, setCurrentTab] = useState('zeplin');
   // Zeplin
   const projectId = '5d3961af5fd2632e5d62fc34';
   const [token, SetToken] = useState('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoicGVyc29uYWxfYWNjZXNzX3Rva2VuIiwiY2xpZW50X2lkIjoiNWU3YTAyYjA3NmNjMjA4YmU1NDljOWE1Iiwic2NvcGUiOiIiLCJpYXQiOjE1ODUwNTQzODQsImV4cCI6MTkwMDYyMzY0NCwiaXNzIjoiemVwbGluOmFwaS56ZXBsaW4uaW8iLCJzdWIiOiI1YzljMTg3NDE2ZDRlZDc3NmJiNTAzNTciLCJqdGkiOiJjZDQwZjFhNC05NzE0LTQzZTItYmNkMS02OTEzYTVjMTRjOWEifQ.3xLhOpEubiXO0GDM2Z6KJ2E9UxRpfjVlqa5Xerr9k5M');
@@ -65,33 +70,43 @@ export default function Home() {
   // Figma
   const [figmaFileID, SetFigmaFileID] = useState('MtR6sYoPw9qnT9VHT6Pwlv');
   const [figmaToken, SetFigmaToken] = useState('39373-5d3aee3e-c57c-4f62-a29c-92af3084aa7f');
-  const [figmaFrameID, SetFigmaFrameID] = useState('5e55658227cd32677b1e6b77');
+  const [figmaFrameID, SetFigmaFrameID] = useState('27:254');
   const [showControlPanel, setShowControlPanel] = useState(false);
+  // image source
+  const [imageSource, setImageSource] = useState({
+    url: '',
+    width: 1024,
+    height: 768,
+  });
 
-  // useEffect(() => {
-  //   fetch(`https://api.zeplin.dev/v1/projects/${projectId}/screens/${screenID}`, { 
-  //     method: 'GET', 
-  //     headers: {
-  //       'Authorization': `Bearer ${token}`, 
-  //       'Accept': 'application/json',
-  //     }
-  //   }).then((res) => {
-  //     return res.json()
-  //   }).then((resJson) => {
-  //     if(resJson.image && resJson.image.original_url) {
-  //       console.log('set to background!', resJson.image);
-  //       const h = 100 * resJson.image.height / resJson.image.width
-  //       document.documentElement.style.setProperty('--overlay-image', `url('${resJson.image.original_url}')`);
-  //       document.documentElement.style.setProperty('--overlay-width', `100%`);
-  //       document.documentElement.style.setProperty('--overlay-height', `${h}vw`);
-  //     }
-  //   }).catch((error) => {
-  //     console.error(error);
-  //   });
-  // }, [screenID])
-
-  useEffect(() => {
-    fetch(`https://api.figma.com/v1/files/${figmaFileID}` , {
+  const handleZeplinStatic = () => {
+    fetch(`https://api.zeplin.dev/v1/projects/${projectId}/screens/${screenID}`, { 
+      method: 'GET', 
+      headers: {
+        'Authorization': `Bearer ${token}`, 
+        'Accept': 'application/json',
+      }
+    }).then((res) => {
+      return res.json()
+    }).then((resJson) => {
+      if(resJson.image && resJson.image.original_url) {
+        console.log('set to background!', resJson.image);
+        const h = 100 * resJson.image.height / resJson.image.width
+        document.documentElement.style.setProperty('--overlay-image', `url('${resJson.image.original_url}')`);
+        document.documentElement.style.setProperty('--overlay-width', `${resJson.image.width}px`);
+        document.documentElement.style.setProperty('--overlay-height', `${resJson.image.height}px`);
+        setImageSource({
+          url: resJson.image.original_url,
+          width: resJson.image.width,
+          height: resJson.image.height,
+        })
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+  const handleFigmaStatic = () => {
+    fetch(`https://api.figma.com/v1/files/${figmaFileID}/nodes?ids=${figmaFrameID}` , {
       method: 'GET',
       headers: {
           'X-Figma-Token': figmaToken
@@ -99,10 +114,60 @@ export default function Home() {
     }).then((res) => {
       return res.json()
     }).then((resJson) => {
-      console.log(resJson);
-    });
+      let nodeInformation = resJson.nodes[figmaFrameID]
+      if (nodeInformation) {
+        const _w = nodeInformation.document.absoluteBoundingBox.width // 100vw
+        const _h = nodeInformation.document.absoluteBoundingBox.height // `${100 * _h / _w}vw`
+        document.documentElement.style.setProperty('--overlay-width', `${_w}px`);
+        document.documentElement.style.setProperty('--overlay-height', `${_h}px`);
+        setImageSource({
+          ...imageSource,
+          width: _w,
+          height: _h,
+        }) 
+      }
+    }).catch(error => console.log(error));
 
+    fetch(`https://api.figma.com/v1/images/${figmaFileID}?format=svg&ids=${figmaFrameID}` , {
+      method: 'GET',
+      headers: {
+          'X-Figma-Token': figmaToken
+      }
+    }).then((res) => {
+      return res.json()
+    }).then((resJson) => {
+      if (resJson && resJson.err === null) {
+        // successful get image
+        if (resJson['images'][figmaFrameID]) {
+          const url = resJson['images'][figmaFrameID]
+          document.documentElement.style.setProperty('--overlay-image', `url('${url}')`);
+          setImageSource({
+            ...imageSource,
+            url,
+          }) 
+        }
+      }
+    }).catch(error => console.log(error));
+  }
+
+  useEffect(() => {
+    if (currentTab === 'Zeplin') {
+      handleZeplinStatic()
+    } else if (currentTab === 'Figma') {
+      handleFigmaStatic()
+    }
+  }, [currentTab]);
+
+  useEffect(() => {
+    handleZeplinStatic()
+  }, [screenID])
+
+  useEffect(() => {
+    handleFigmaStatic()
   }, [figmaFrameID])
+
+  console.log(imageSource);
+  
   const ControlPanelButton = (
     <EuiButton
       iconSide="right"
@@ -156,11 +221,41 @@ export default function Home() {
     {
       id: 'figma--id',
       name: 'Figma',
-      disabled: true,
+      // disabled: true,
       content: (
-        <React.Fragment>
-         TBD
-        </React.Fragment>
+        <EuiFlexGroup style={{ maxWidth: 600, marginTop: 8 }}>
+          <EuiFlexItem>
+            <EuiFormRow id="section-figma-fileID">
+              <EuiFieldText
+                compressed
+                placeholder="get it from the figma url"
+                value={figmaFileID}
+                prepend="File ID"
+                readOnly
+              />
+            </EuiFormRow>
+
+            <EuiFormRow id="section-figma-frameID">
+              <EuiFieldText
+                compressed
+                placeholder="get it from the figma url"
+                value={figmaFrameID}
+                prepend="Frame ID"
+                onChange={(e) => SetFigmaFrameID(e.target.value)}
+              />
+            </EuiFormRow>
+
+            <EuiFormRow id="section-figma-token">
+              <EuiFieldText
+                compressed
+                placeholder="get it from the develop panel on figma"
+                value={figmaToken}
+                prepend="Token"
+                onChange={(e) => SetFigmaToken(e.target.value)}
+              />
+            </EuiFormRow>
+          </EuiFlexItem>
+        </EuiFlexGroup>
       ),
     },  
   ]
@@ -178,27 +273,13 @@ export default function Home() {
           <EuiForm component="form">
             <EuiFlexGroup style={{ maxWidth: 600 }}>
               <EuiFlexItem>
-                {/* <EuiFormRow label="Scale" helpText="">
-                    <EuiRange
-                      id={'123'}
-                      min={0}
-                      max={100}
-                      step={1}
-                      value={opacity}
-                      onChange={e => {
-                        const newV = parseInt(e.target.value, 10)
-                        SetOpacity(newV);
-                        document.documentElement.style.setProperty('--overlay-opacity', `${newV / 100}`);
-                      }}
-                      showLabels
-                  />
-                </EuiFormRow> */}
                 <EuiFormRow id="overlay-opacity-control" label="Opacity" helpText="">
                   <EuiRange
                     id={'123'}
                     min={0}
                     max={100}
                     step={1}
+                    showValue
                     value={opacity}
                     onChange={e => {
                       const newV = parseInt(e.target.value, 10)
@@ -207,6 +288,25 @@ export default function Home() {
                     }}
                     showLabels
                   />
+                  </EuiFormRow>
+
+                  <EuiFormRow id="overlay-scale-control" label="Scale" helpText="">
+                    <EuiRange
+                      id={'scale'}
+                      min={40}
+                      max={160}
+                      showValue
+                      step={20}
+                      value={scale}
+                      onChange={e => {
+                        const newScale = parseInt(e.target.value, 10)
+                        SetScale(newScale);
+                        const _scale = newScale / 100
+                        document.documentElement.style.setProperty('--overlay-width', `${imageSource.width * _scale}px`);
+                        document.documentElement.style.setProperty('--overlay-height', `${imageSource.height * _scale}px`);
+                      }}
+                      showLabels
+                    />
                   </EuiFormRow>
               </EuiFlexItem>
             </EuiFlexGroup>
@@ -221,6 +321,7 @@ export default function Home() {
                     autoFocus="selected"
                     onTabClick={tab => {
                       console.log('clicked tab', tab);
+                      setCurrentTab(tab.name);
                     }}
                     style={{
                       maxWidth: _w,
